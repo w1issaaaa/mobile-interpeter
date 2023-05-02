4a9dade2b858997d66b896ce1edd2bd9fb0c2977
w1issaaaa <wlissaaaa@icloud.com>
2023-05-03 01:00:07 +0700

    Initial Commit

---
+++ CodeInterpreter/CodeInterpreterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeInterpreterApp.swift
+//  CodeInterpreter
+//
+//  Created by Lisa Bezrukova on 03.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct CodeInterpreterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
